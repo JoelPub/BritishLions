53273f1423f30c01d79cd2b6ce5d405845944267
Joel <jwang2@apdgroup.com>
2016-11-23 16:24:03 +0800

    LIONS-207: Union detail page UI development

--- contents/unions/data.js
+++ contents/unions/data.js
@@ -4,27 +4,39 @@ export default [
         "displayname": "England",
         "shortname": "Eng",
         "image": "https:\/\/cdn.soticservers.net\/tools\/images\/teams\/logos\/250x250\/125.png",
-        "logo": require("./nations/england.png")
+        "logo": require("./nations/england.png"),
+        "url": "http://www.englandrugby.com",
+        "images": [{"source":require("./england.png")}],
+        "description": "<p>The Rugby Football Union is the national governing body for grassroots and elite rugby in England, with 2,000 autonomous rugby clubs in its membership.</p><p>The clubs are grouped within 35 Constituent Bodies (CBs), comprised of counties – some individual, some combined – the three armed forces, Oxford and Cambridge Universities, England Schools Rugby Football Union and England Students.</p><p>All of this is supported by the RFU's 50 Rugby Development Officers, six Area Managers and 120 Community Rugby Coaches across the country, who provide some 30,000 coaching sessions a year for young people.</p><p>The RFU employs approximately 500 paid staff and helps to train and support more than 60,000 volunteers whose roles include:</p><ul><li>Organising rugby activity, including the playing, coaching and refereeing of matches and recreational rugby at all levels</li><li>Supporting the volunteer workforce</li><li>Working with clubs to secure grants and loans for facilities</li><li>Fundraising, handling money and insurance</li><li>Offering medical advice and support</li><li>Committee member/trustee</li><li>Secretarial, administration and clerical help</li></ul>"
     },
     {
         "id": "126",
         "displayname": "Ireland",
         "shortname": "Ire",
         "image": "https:\/\/cdn.soticservers.net\/tools\/images\/teams\/logos\/250x250\/126.png",
-        "logo": require("./nations/ireland.png")
+        "logo": require("./nations/ireland.png"),
+        "url": "http://www.irishrugby.ie",
+        "images": [{"source":require("./ireland.png")},{"source":require("./irfu.png")}],
+        "description": "<p>The Irish Rugby Football Union is the governing body for the sport of Rugby Union on the island of Ireland. It was founded in 1879</p><p>The IRFU has its head office in Dublin, and home ground at Aviva Stadium, where adult men's Irish rugby union international matches are played.</p>"
     },
     {
         "id": "127",
         "displayname": "Scotland",
         "shortname": "Sco",
         "image": "https:\/\/cdn.soticservers.net\/tools\/images\/teams\/logos\/250x250\/127.png",
-        "logo": require("./nations/scotland.png")
+        "logo": require("./nations/scotland.png"),
+        "url": "http://www.scottishrugby.org",
+        "images": [{"source":require("./scotland.png")}],
+        "description": "<p>Scottish Rugby is the governing body for rugby union in Scotland.</p><p>It aims to inspire Scotland through rugby, and everyone involved in playing, delivering and organising rugby has a role to play in helping the sport to grow and flourish. </p><p>Based at BT Murrayfield Stadium, Edinburgh, it administers Scotland's international rugby teams, and Scotland's two professional clubs, Edinburgh Rugby and Glasgow Warriors. As well as international rugby, Scottish Rugby supports the domestic game in local rugby clubs and schools across the country. </p><p>Scotland has a proud heritage within the world game. </p><p>The first ever rugby international match was played at Raeburn Place, Edinburgh, in 1871 on the cricket field of The Edinburgh Academy between Scotland and England.</p><p>This preceded the creation of The Scottish Football Union - as it was then called -  which was founded in 1873.</p><p>In 1897, the SFU bought land at Inverleith, Edinburgh which was the home of rugby in Scotland before the first Murrayfield Stadium was built in 1925. The current home of international rugby in Scotland is BT Murrayfield Stadium. </p><p>Scotland is also the birthplace of rugby sevens, invented in 1883 by Ned Haig and David Sanderson as a fundraising event for their local club, Melrose RFC.</p><p>Scotland has a long history of contributing players and coaches to the British & Irish Lions.  Leading players have included Andy Irvine, Ian McLauchlan, Gordon Brown, Gavin Hastings among many others, while Sir Ian McGeechan was Lions head coach in 1989, 1993, 1997 and 2009.</p>"
     },
     {
         "id": "128",
         "displayname": "Wales",
         "shortname": "Wal",
         "image": "https:\/\/cdn.soticservers.net\/tools\/images\/teams\/logos\/250x250\/128.png",
-        "logo": require("./nations/wales.png")
+        "logo": require("./nations/wales.png"),
+        "url": "http://www.wru.co.uk/eng/home.php",
+        "images": [{"source":require("./wales.png")}],
+        "description": "<p>The WRU have been the guardians of Wales's national sport since 1881. A group of 11 clubs - Swansea, Lampeter, Llandeilo, Cardiff, Newport, Llanelli, Merthyr, Llandovery, Brecon, Pontypool and Bangor - came together at the Castle Hotel, Neath on 12th March 1881, to form the Welsh Rugby Football Union. The first Triple Crown came in 1893 and was the launch pad for the first 'Golden Era', when Wales dominated the world game.</p><p>The first 'Golden Era' included the first Grand Slam by any country and a record winning run of 11 games as Wales remained unbeaten between March 1907 to January 1910. When Rob Howley's side of 1999 matched that feat they did it in eight months. There were Grand Slams in 1908, 1909 and 1911, Triple Crowns in 1900, 1902, 1905, 1908, 1909 and 1911and those victories over New Zealand in 1905 and Australia in 1908. Welsh rugby had arrived as a major force in the world game.</p><p>In the modern era, the WRU as a business blends the traditional with the modern; with over 125 years of rugby tradition and heritage behind us. The benefits of the restructure of professional rugby have reaped huge rewards since 2003 with three Six Nations Grand Slams, in 2005, 2008 and 2012, and a fourth championship title in 2013</p>"
     } 
 ]
--- js/components/unions/details.js
+++ js/components/unions/details.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Image, View, Platform } from 'react-native'
+import { Image, View, Platform, WebView } from 'react-native'
 import { Container, Content, Text, Icon } from 'native-base'
 import theme from '../../themes/base-theme'
 import styles from './styles'
@@ -19,10 +19,14 @@ import { Grid, Col, Row } from 'react-native-easy-grid'
 import Swiper from 'react-native-swiper'
 import Lightbox from 'react-native-lightbox'
 import Slider from '../utility/imageSlider'
+import styleVar from '../../themes/variable'
 
 class UnionDetails extends Component {
     constructor(props){
         super(props)
+        this.state={
+          height:0,
+        }
     }
 
     render() {
@@ -52,115 +56,41 @@ class UnionDetails extends Component {
                                     source={this.props.details.logo}
                                     style={styles.banner} />
                             </Lightbox>
-                            <Lightbox navigator={this.props.navigator}>
-                                <Image transparent
-                                    resizeMode='contain'
-                                    source={require('../../../contents/unions/england.png')}
-                                    style={styles.banner} />
-                            </Lightbox>
-                            <Lightbox navigator={this.props.navigator}>
-                                <Image transparent
-                                    resizeMode='contain'
-                                    source={require('../../../contents/unions/ireland.png')}
-                                    style={styles.banner} />
-                            </Lightbox>
+                            {
+                                this.props.details.images.map((item,index)=>{
+                                    return (
+                                    <Lightbox key={index} navigator={this.props.navigator}>
+                                        <Image transparent
+                                            resizeMode='contain'
+                                            source={item.source}
+                                            style={styles.banner} />
+                                    </Lightbox>
+                                    )
+                                })
+                            }
 
                             </Swiper>
 
                             <View style={styles.shareLinkWrapper}>
-                                <ExternalLink style={styles.shareLink} url='https://www.lionsrugby.com'>
+                                <ExternalLink style={styles.shareLink} url={this.props.details.url}>
                                     <Text style={styles.shareLinkText}><Icon name='md-open' style={styles.shareLinkIcon} /> VISIT {this.props.details.displayname.toUpperCase()} RUGBY WEBSITE </Text>
                                 </ExternalLink>
                             </View>
 
-                            <View style={styles.description}>
-                                <Text style={[staticStyles.pageText]}>
-                                    The Rugby Football Union is the national governing body for grassroots and elite rugby in England, with 2,000 autonomous rugby clubs in its membership.
-                                </Text>
-                                <Text style={[staticStyles.pageText]}>
-                                    The clubs are grouped within 35 Constituent Bodies (CBs), comprised of counties – some individual, some combined – the three armed forces, Oxford and Cambridge Universities, England Schools Rugby Football Union and England Students.
-                                </Text>
-                                <Text style={[staticStyles.pageText]}>
-                                    All of this is supported by the RFU's 50 Rugby Development Officers, six Area Managers and 120 Community Rugby Coaches across the country, who provide some 30,000 coaching sessions a year for young people.
-                                </Text>
-                                <Text style={[staticStyles.pageText]}>
-                                    The RFU employs approximately 500 paid staff and helps to train and support more than 60,000 volunteers whose roles include:
-                                </Text>
-
-                                <View style={staticStyles.gridWrapper}>
-                                    <Grid>
-                                        <Row size={2}>
-                                            <Col style={{width:20}}>
-                                                <Text style={[staticStyles.pageText]}>•</Text>
-                                            </Col>
-                                            <Col>
-                                                <Text style={[staticStyles.pageText]}>
-                                                    Organising rugby activity, including the playing, coaching and refereeing of matches and recreational rugby at all levels
-                                                </Text>
-                                            </Col>
-                                        </Row>
-                                         <Row size={1} style={{marginTop:-20}}>
-                                            <Col style={{width:20}}>
-                                                <Text style={[staticStyles.pageText]}>•</Text>
-                                            </Col>
-                                            <Col>
-                                                <Text style={[staticStyles.pageText]}>
-                                                    Supporting the volunteer workforce
-                                                </Text>
-                                            </Col>
-                                        </Row>
-                                         <Row size={1} style={Platform.OS==='android'?{marginTop:-10}:{marginTop:-30}}>
-                                            <Col style={{width:20}}>
-                                                <Text style={[staticStyles.pageText]}>•</Text>
-                                            </Col>
-                                            <Col>
-                                                <Text style={[staticStyles.pageText]}>
-                                                    Working with clubs to secure grants and loans for facilities
-                                                </Text>
-                                            </Col>
-                                        </Row>
-                                         <Row size={1} style={{marginTop:-10}}>
-                                            <Col style={{width:20}}>
-                                                <Text style={[staticStyles.pageText]}>•</Text>
-                                            </Col>
-                                            <Col>
-                                                <Text style={[staticStyles.pageText]}>
-                                                    Fundraising, handling money and insurance
-                                                </Text>
-                                            </Col>
-                                        </Row>
-                                         <Row size={1} style={{marginTop:-10}}>
-                                            <Col style={{width:20}}>
-                                                <Text style={[staticStyles.pageText]}>•</Text>
-                                            </Col>
-                                            <Col>
-                                                <Text style={[staticStyles.pageText]}>
-                                                    Offering medical advice and support
-                                                </Text>
-                                            </Col>
-                                        </Row>
-                                         <Row size={1} style={Platform.OS==='android'?{marginTop:-10}:{marginTop:-30}}>
-                                            <Col style={{width:20}}>
-                                                <Text style={[staticStyles.pageText]}>•</Text>
-                                            </Col>
-                                            <Col>
-                                                <Text style={[staticStyles.pageText]}>
-                                                    Committee member/trustee
-                                                </Text>
-                                            </Col>
-                                        </Row>
-                                         <Row size={1} style={{marginTop:-30}}>
-                                            <Col style={{width:20}}>
-                                                <Text style={[staticStyles.pageText]}>•</Text>
-                                            </Col>
-                                            <Col>
-                                                <Text style={[staticStyles.pageText]}>
-                                                    Secretarial, administration and clerical help
-                                                </Text>
-                                            </Col>
-                                        </Row>
-                                    </Grid>
-                                </View>
+                            <View style={[styles.description,{height:this.state.height}]}>
+                                <WebView
+                                    style={{flex:1}}
+                                    bounces={false}
+                                    scrollEnabled={false}
+                                    source={{html:`<!DOCTYPE html><html><head><style>body{width:${parseInt(styleVar.deviceWidth)-50}px;}p{font-size: 18px;font-family: 'georgia';line-height: 24px;color: rgb(38,38,38);margin-bottom: 20px;}ul{font-size: 18px;line-height: 24px;}li{font-size: 18px;font-family: 'georgia';line-height: 24px;color: rgb(38,38,38);}</style></head><body>${this.props.details.description}<script>window.onload=function(){window.location.hash = 1;document.title = document.body.clientHeight;}</script></body></html>`}}
+                                    onNavigationStateChange={(title)=>{
+                                        if(title.title!== undefined && title.title.trim()!==''&&isNaN(title.title)===false) {
+                                            this.setState({
+                                                height:(parseInt(title.title)+150)
+                                            })
+                                        }
+                                    }}
+                                />
                                 <PaginationButton label='NEXT UNION' style={styles.paginateButton} next={true} data={[this.props.details.id, 'unionDetails', false]} />
                             </View>
                         </View>
