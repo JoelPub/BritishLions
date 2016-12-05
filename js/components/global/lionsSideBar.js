3c753cefaddb63b4d4e46778f0d487a970413cf7
roy anonuevo <ranonuevo@apdgroup.com>
2016-12-05 11:04:53 +0800

    LIONS-86 Redirect to login page when non logged in user trying to access the my lions page

--- js/components/global/lionsSideBar.js
+++ js/components/global/lionsSideBar.js
@@ -106,6 +106,14 @@ class LionsSidebar extends Component {
         }, 400)
         this.props.closeDrawer()
     }
+
+    _requireSignIn(route) {
+        if (this.props.isAccessGranted) {
+            this.navigateTo(route)
+        } else {
+            this.navigateTo('login')
+        }
+    }
     
     shouldComponentUpdate(nextProps, nextState) {
         return true
@@ -146,18 +154,10 @@ class LionsSidebar extends Component {
                         <Text style={styles.linkText}>GALLERIES</Text>
                         <Icon name='md-image' style={styles.icon} />
                     </ButtonFeedback>
-                    
-                    { 
-                        this.props.isAccessGranted?
-                            <ButtonFeedback onPress={() => this.navigateTo('myLions')} style={styles.links}>
-                                <Text style={styles.linkText}>MY LIONS</Text>
-                                <Icon name='md-heart' style={styles.icon} />
-                            </ButtonFeedback>
-                        : 
-                            null
-                    }
-
-
+                    <ButtonFeedback onPress={() => this._requireSignIn('myLions')} style={styles.links}>
+                        <Text style={styles.linkText}>MY LIONS</Text>
+                        <Icon name='md-heart' style={styles.icon} />
+                    </ButtonFeedback>
                     <ButtonFeedback onPress={() => this.navigateTo('lionsStore')} style={styles.links}>
                         <Text style={styles.linkText}>OFFICIAL STORE</Text>
                         <Icon name='md-ribbon' style={styles.icon} />
