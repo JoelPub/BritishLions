472477aa65065b1902d30e0ddda20a870537b83a
Yilun Yang <yyang@apdgroup.com>
2016-12-28 19:35:26 +1100

    LIONS-497:implemented logic of the my squad

--- js/components/global/eySponsoredFooter.js
+++ js/components/global/eySponsoredFooter.js
@@ -1,9 +1,11 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { Image, View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { Image, View, Text, TouchableOpacity } from 'react-native'
 import { styleSheetCreate } from '../../themes/lions-stylesheet'
 import styleVar from '../../themes/variable'
+import { pushNewRoute } from '../../actions/route'
 
 const styles = styleSheetCreate({
   sponsoredEYContainer:{
@@ -14,13 +16,12 @@ const styles = styleSheetCreate({
       alignItems: 'flex-end',
       justifyContent: 'flex-end',
       opacity: 0.95,
-      paddingBottom: 8
   },
   sponsoredEYLogo:{
       height: 30,
       width: 29,
       marginRight: 15,
-      marginBottom: 3
+      marginBottom: 11
   },
   sponsoredEYText: {
       textAlign: 'right',
@@ -31,25 +32,37 @@ const styles = styleSheetCreate({
       width: 100,
       opacity: 0.75,
       color: '#FFF',
-
+      paddingBottom: 8
+  },
+  mySquadBtn:{
+       flexDirection:'row',
+       borderRightWidth:1,
+       height:50,
+       paddingTop:8,
+       paddingRight:15,
+       paddingBottom: 8,
+       borderRightColor:'rgba(255,255,255,0.15)'
   }
 })
 
-export default class EYFooter extends Component {
+export class EYFooter extends Component {
+      _mySquad(){
+             this.props.pushNewRoute('mySquad')
+      }
 
 	  render() {
         return (
 
               <View style={styles.sponsoredEYContainer}>
                 {this.props.mySquadBtn&&
-                <View style={{flexDirection:'row'}}>
+                <TouchableOpacity style={styles.mySquadBtn} onPress={() => this._mySquad()}>
                     <Image
                         resizeMode='contain'
                         transparent
                         source={require('../../../images/header/logo.png')}
                         style={{width: 27, height: 34, marginLeft: 10,  backgroundColor: 'transparent', }} />
-                        <Text style={{fontFamily: styleVar.fontCondensed,color:'rgb(255,255,255)',fontSize:24,marginTop:5,paddingLeft:5}}>MY SQUAD</Text>
-                </View>
+                        <Text style={{fontFamily: styleVar.fontCondensed,color:'rgb(255,255,255)',fontSize:24,marginTop:8,paddingLeft:5}}>MY SQUAD</Text>
+                </TouchableOpacity>
                 }
                 <Text style={styles.sponsoredEYText}>Sponsored by</Text>
                 <Image source={require('../../../images/footer/eyLogo.png')} style={styles.sponsoredEYLogo}></Image>
@@ -59,3 +72,14 @@ export default class EYFooter extends Component {
         )
     }
 }
+function bindAction(dispatch) {
+    return {
+        pushNewRoute: (route)=>dispatch(pushNewRoute(route))
+    }
+}
+
+export default connect((state) => {
+    return {
+    }
+},  bindAction)(EYFooter)
+
