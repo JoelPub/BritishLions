2f0fda4c10c9751791463466bf05d5e8f367a42f
周贤 <zhouxian@xiandeMacBook-Pro.local>
2017-02-02 13:27:24 +0800

    notice working

--- js/components/utility/notification.js
+++ js/components/utility/notification.js
@@ -13,13 +13,13 @@ export const configure= new Promise((resolve,reject)=>{
 export function schedule() {
         let startDate=new Date()
         let offset=startDate.getTimezoneOffset()
-        // console.log('!!!startDate',startDate)
-        //console.log('startDate', startDate)
+         console.log('!!!startDate',startDate)
+        console.log('startDate', startDate)
         scheduleList.map((item,index)=>{
         	let targetDate = new Date(Date.UTC(item.year, item.month, item.day, item.hour, item.minute, item.second))
-        	//console.log(targetDate, item.message)
-            // console.log('!!!',targetDate-startDate)
-            // console.log('!!!',targetDate-startDate+offset*60*1000)
+        	console.log(targetDate, item.message)
+             console.log('!!!',targetDate-startDate)
+             console.log('!!!',targetDate-startDate+offset*60*1000)
             if(targetDate-startDate>0) {
 	        	PushNotification.localNotificationSchedule({
                   title: 'Lions Official',
