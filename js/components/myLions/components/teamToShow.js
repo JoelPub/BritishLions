9639af786debb6d59a5f0920ffa3efef9fb02a75
Joel <jwang2@apdgroup.com>
2017-02-24 14:46:44 +0800

    LIONS-733

--- js/components/myLions/components/teamToShow.js
+++ js/components/myLions/components/teamToShow.js
@@ -2,6 +2,7 @@
 
 import TeamShowModel from  '../../../modes/Team/TeamShowModel'
 import { strToUpper } from '../../utility/helper'
+import { searchPlayer } from './searchPlayer'
 export function compareShowSquad(rSquad,jSquad){
     if (JSON.stringify(jSquad)==='{}') return false
     let result=true
@@ -87,39 +88,6 @@ export function checkFullTeam(teamShow) {
     }
     return result    
 }
-
-function  searchPlayer(player,id,uniondata) {
-        let result=null
-        for(let union in player) {
-            result=player[union].find((item)=>item.id===id.toString())
-            if(result !== undefined) {
-                let unionInfo = uniondata.find((n)=> n.id===union)
-                Object.assign(result, {
-                    logo: unionInfo.image, 
-                    country: unionInfo.displayname.toUpperCase(),
-                    countryid: unionInfo.id
-                })
-
-                if(typeof result.image==='string') {
-                   if (result.image.indexOf('125.gif') > 0) {
-                        result.image = require(`../../../contents/unions/nations/125.png`)
-                    } else if (result.image.indexOf('126.gif') > 0) {
-                        result.image = require(`../../../contents/unions/nations/126.png`)
-                    } else if (result.image.indexOf('127.gif') > 0) {
-                        result.image = require(`../../../contents/unions/nations/127.png`)
-                    } else if (result.image.indexOf('128.gif') > 0) {
-                        result.image = require(`../../../contents/unions/nations/128.png`)
-                    } else {
-                        result.image = {uri:result.image}
-                    } 
-                }
-                if(strToUpper(result.position)==='FLANKER'||strToUpper(result.position)==='NO. 8') result.position='Back Row'
-                if(strToUpper(result.position)==='UTILITY BACK') result.position='Full Back'
-                return result
-            }
-        }
-        return result===undefined?null:result
-    }
 
 export function removePlayer(squadToShow,position,playerid) {
     if(playerid) {
