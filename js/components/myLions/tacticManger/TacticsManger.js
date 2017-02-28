c26a3c103da8b46998f94299fc630815c3f8947b
周贤 <zhouxian@xiandeMacBook-Pro.local>
2017-02-28 15:27:10 +0800

    LIONS-738 create Tactics

---
+++ js/components/myLions/tacticManger/TacticsManger.js
@@ -0,0 +1,106 @@
+
+
+'use strict'
+
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Image, View, ScrollView, ActivityIndicator} from 'react-native'
+import { Container, Content, Text, Button, Icon, Input } from 'native-base'
+import { Grid, Col, Row } from 'react-native-easy-grid'
+import LinearGradient from 'react-native-linear-gradient'
+import Swiper from 'react-native-swiper'
+
+import theme from '../../../themes/base-theme'
+import styles from './styles'
+import shapes from '../../../themes/shapes'
+import styleVar from '../../../themes/variable'
+import loader from '../../../themes/loader-position'
+
+import LoginRequire from '../../global/loginRequire'
+import LionsHeader from '../../global/lionsHeader'
+import LionsFooter from '../../global/lionsFooter'
+
+import ImagePlaceholder from '../../utility/imagePlaceholder'
+import ButtonFeedback from '../../utility/buttonFeedback'
+import ImageCircle from '../../utility/imageCircle'
+import { replaceRoute, pushNewRoute } from '../../../actions/route'
+import EYSFooter from '../../global/eySponsoredFooter'
+
+import { drillDown } from '../../../actions/content'
+import { globalNav } from '../../../appNavigator'
+
+import HeaderTitleWithModal from '../components/HeaderTitleWithModal'
+import Versus from '../components/versus'
+
+
+
+
+
+class TacticsManger extends Component {
+  constructor (props) {
+    super(props)
+    this._scrollView = ScrollView
+    this.isUnMounted = false
+    this.state = {
+      isLoaded: false,
+      drillDownItem: this.props.drillDownItem
+    }
+  }
+  /*get Data*/
+
+
+
+
+  /*router logic*/
+
+
+
+  render() {
+    let { isLoaded } = this.state
+    let {userProfile} = this.props
+    return (
+      <Container theme={theme}>
+        <View style={styles.container}>
+          <LionsHeader
+            back={true}
+            title='MY LIONS'
+            contentLoaded={true}
+            scrollToTop={ ()=> { this._scrollView.scrollTo({ y: 0, animated: true })}} />
+          <ScrollView ref={(scrollView) => { this._scrollView = scrollView }}>
+            <HeaderTitleWithModal title={'SELECT TACTICS'}/>
+            <Versus gameData={this.state.drillDownItem} userData={userProfile} />
+
+            <LionsFooter isLoaded={true} />
+          </ScrollView>
+          <LoginRequire/>
+          <EYSFooter mySquadBtn={true}/>
+        </View>
+      </Container>
+    )
+  }
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+    this.isUnMounted = true
+  }
+}
+
+
+function bindAction(dispatch) {
+  return {
+    drillDown: (data, route)=>dispatch(drillDown(data, route)),
+    replaceRoute:(route)=>dispatch(replaceRoute(route)),
+  }
+}
+export default connect((state) => {
+  console.log(state)
+  return {
+    route: state.route,
+    drillDownItem: state.content.drillDownItem,
+    userProfile:state.squad.userProfile
+  }
+}, bindAction)(TacticsManger)
+TacticsManger.defaultProps = {
+
+}
