e3669ed95082e8e6a6286e3e33114334fc6fcc43
周贤 <zhouxian@xiandeMacBook-Pro.local>
2017-03-01 10:09:20 +0800

    LIONS-738  add component   react-native-modal-dropdown

--- js/components/myLions/tacticManger/TacticsManger.js
+++ js/components/myLions/tacticManger/TacticsManger.js
@@ -20,6 +20,7 @@ import LoginRequire from '../../global/loginRequire'
 import LionsHeader from '../../global/lionsHeader'
 import LionsFooter from '../../global/lionsFooter'
 
+import ModalDropdown from 'react-native-modal-dropdown';
 import ImagePlaceholder from '../../utility/imagePlaceholder'
 import ButtonFeedback from '../../utility/buttonFeedback'
 import ImageCircle from '../../utility/imageCircle'
@@ -32,18 +33,23 @@ import { globalNav } from '../../../appNavigator'
 import HeaderTitleWithModal from '../components/HeaderTitleWithModal'
 import Versus from '../components/versus'
 
+
+const DEMO_OPTIONS_1 = ['option 1', 'option 2', 'option 3', 'option 4', 'option 5'];
+
 class SmallBox extends Component {
   constructor (props) {
     super(props)
     this.state = {}
   }
     render (){
+      let{height}=this.props
+      const dimensions = { height };
       return(
         <View style={styles.container}>
           <View style={styles.header}>
             <Text style={styles.headerText}>{this.props.title}</Text>
           </View>
-          <View>
+          <View style={[styles.smallBoxContent,dimensions]}>
             {this.props.children}
           </View>
         </View>
@@ -51,14 +57,13 @@ class SmallBox extends Component {
     }
 }
 SmallBox.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  height: React.PropTypes.number,
 }
-const StarPlayer = () => {
-    return (
-      <View>
-      </View>
-    )
-}
+
+SmallBox.defaultProps = {
+  height: 120,
+};
 class TacticsManger extends Component {
   constructor (props) {
     super(props)
@@ -89,7 +94,11 @@ class TacticsManger extends Component {
           <ScrollView ref={(scrollView) => { this._scrollView = scrollView }}>
             <HeaderTitleWithModal title={'SELECT TACTICS'}/>
             <Versus gameData={this.state.drillDownItem} userData={userProfile} />
-            <SmallBox title={'STAR PLAYER'} />
+            <SmallBox title={'STAR PLAYER'} >
+              <ModalDropdown style={styles.dropDown}
+                             defaultValue={'Select Player'}
+                             options={DEMO_OPTIONS_1} />
+            </SmallBox>
             <LionsFooter isLoaded={true} />
           </ScrollView>
           <LoginRequire/>
--- js/components/myLions/tacticManger/styles.js
+++ js/components/myLions/tacticManger/styles.js
@@ -25,5 +25,17 @@ module.exports = styleSheetCreate({
     textAlign: 'center',
     fontFamily: styleVar.fontGeorgia
   },
-
+  smallBoxContent: {
+    flex: 1,
+    backgroundColor: 'rgb(239,239,240)',
+    paddingHorizontal:26
+  },
+  dropDown: {
+    height: 40,
+    borderRadius:1,
+    borderWidth: 1,
+    borderColor: 'rgb(216, 217, 218)',
+    backgroundColor: 'rgb(255,255,255)',
+    marginTop: 30
+  }
 })
