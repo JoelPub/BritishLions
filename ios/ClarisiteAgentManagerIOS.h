782d1208de87b2c2b53d8079b5ba2980a4e7b1a2
周贤 <zhouxian@xiandeMacBook-Pro.local>
2017-03-03 16:47:22 +0800

    LIONS-197 IOS  configuration

---
+++ ios/ClarisiteAgentManagerIOS.h
@@ -0,0 +1,13 @@
+//
+//  ClarisiteAgentManagerIOS.h
+//  LionsOfficial
+//
+//  Created by 周贤 on 2017/3/3.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ClarisiteAgentManagerIOS : NSObject
+
+@end
---
+++ ios/ClarisiteAgentManagerIOS.m
@@ -0,0 +1,13 @@
+//
+//  ClarisiteAgentManagerIOS.m
+//  LionsOfficial
+//
+//  Created by 周贤 on 2017/3/3.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import "ClarisiteAgentManagerIOS.h"
+
+@implementation ClarisiteAgentManagerIOS
+
+@end
