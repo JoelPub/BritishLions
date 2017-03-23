85ad40eefb56dae5efb239eee23184f45f828c0c
Joel <jwang2@apdgroup.com>
2017-03-23 11:00:33 +0800

    LIONS-883

--- js/components/myLions/myLionsCoachProfile/myLionsCoachProfile.js
+++ js/components/myLions/myLionsCoachProfile/myLionsCoachProfile.js
@@ -3,7 +3,7 @@
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Image, View, Platform, Alert,  ScrollView, Modal, ActivityIndicator } from 'react-native'
+import { Image, View, Platform, Alert,  ScrollView, Modal, ActivityIndicator,WebView } from 'react-native'
 import { Container, Thumbnail, Header, Title, Text, Button, Icon } from 'native-base'
 import { Grid, Col, Row } from 'react-native-easy-grid'
 import LinearGradient from 'react-native-linear-gradient'
@@ -17,70 +17,17 @@ import LionsFooter from '../../global/lionsFooter'
 import ImageCircle from '../../utility/imageCircle'
 import ButtonFeedback from '../../utility/buttonFeedback'
 import { pushNewRoute, replaceRoute } from '../../../actions/route'
-import { setAccessGranted } from '../../../actions/token'
-import { removeToken } from '../../utility/asyncStorageServices'
-import { service } from '../../utility/services'
-import HTMLView from 'react-native-htmlview'
-import htmlStyles from '../../../themes/html-styles'
 import styleVar from '../../../themes/variable'
-import { getGoodFormFavoritePlayerList, removeGoodFormFavoritePlayerList } from '../../utility/apiasyncstorageservice/goodFormAsyncStorageService'
-import SquadModal from '../../global/squadModal'
-import PlayerFigure from '../../global/playerFigure'
-import { getUserCustomizedSquad, removeUserCustomizedSquad } from '../../utility/apiasyncstorageservice/goodFormAsyncStorageService'
-import { getAssembledUrl } from '../../utility/urlStorage'
-import ProfileListModel from  '../../../modes/Players'
-import ProfileModel from '../../../modes/Players/Profile'
-import FigureListModel from  '../../../modes/Players/Profile/SeasonList/Season/FigureList'
-import FigureModel from '../../../modes/Players/Profile/SeasonList/Season/FigureList/Figure'
-import SquadModel from  '../../../modes/Squad'
-import SquadShowModel from  '../../../modes/Squad/SquadShowModel'
-import Immutable, { Map, List } from 'immutable'
 import { strToUpper } from '../../utility/helper'
 import ImagePlaceholder from '../../utility/imagePlaceholder'
-import Swiper from 'react-native-swiper'
-import SquadList from '../../global/squadList'
-import { setSquadToShow,setSquadData } from '../../../actions/squad'
-import {removePlayer,addPlayer,replacePlayer,checkFullSquad} from '../../global/squadToShow'
 
 
 class MyLionsCoachProfile extends Component {
     constructor(props){
         super(props)
         this._scrollView = ScrollView
-        this.playerid = this.props.detail.id,
-        this.playerName = this.props.detail.name,
-        this.playerPos=null,
-        this.seq=0
-    }
-
-    _replaceRoute(route) {
-        this.props.replaceRoute(route)
-    }
-
-    _reLogin() {
-        removeToken()
-        this.props.setAccessGranted(false)
-        this._replaceRoute('login')
-    }
-
-    _signInRequired() {
-        Alert.alert(
-            'Your session has expired',
-            'Please sign into your account.',
-            [{
-                text: 'SIGN IN', 
-                onPress: this._reLogin.bind(this)
-            }]
-        )
-    }
-
-    _showError(error) {
-         if(error !== ''){
-            Alert.alert(
-                'An error occured',
-                error,
-                [{text: 'Dismiss'}]
-            )
+        this.state={
+          height:0,
         }
     }
 
@@ -122,22 +69,23 @@ class MyLionsCoachProfile extends Component {
                                 <Text style={styles.detailsLabel}>UNION</Text>
                                 <Text style={styles.detail}>{this.props.detail.education} </Text>
                             </Col>
-                            <Col style={styles.detailsGridCol} size={1}>
-                                <Text style={styles.detail}>{this.props.detail.biog} </Text>
-                            </Col>
                         </Grid>
-                        {/*
-                            this.props.detail.biog?
-                                <View style={styles.playerDesc}>
-                                    <HTMLView
-                                       value={this.props.detail.biog}
-                                       stylesheet={htmlStyles}
-                                     />
-                                </View>
-                            :
-                                null
 
-                        */}
+                        <View style={[styles.playerDesc,{height:this.state.height}]}>
+                            <WebView
+                                style={{flex:1}}
+                                bounces={false}
+                                scrollEnabled={false}
+                                source={{html:`<!DOCTYPE html><html><head><style>body{width:${parseInt(styleVar.deviceWidth)-50}px;}h1{font-size: 18px;font-family: 'georgia';line-height: 18px;color: rgb(38,38,38);margin-top: 20px;margin_bottom:0;font-weight:bold}p{font-size: 16px;font-family: 'georgia';line-height: 22px;color: rgb(38,38,38);margin-top: -5px;margin_bottom:0;}ul{font-size: 18px;line-height: 24px;}li{font-size: 18px;font-family: 'georgia';line-height: 24px;color: rgb(38,38,38);}</style></head><body>${this.props.detail.biog}<script>window.onload=function(){window.location.hash = 1;document.title = document.body.clientHeight;}</script></body></html>`}}
+                                onNavigationStateChange={(title)=>{
+                                    if(title.title!== undefined && title.title.trim()!==''&&isNaN(title.title)===false) {
+                                        this.setState({
+                                            height:(parseInt(title.title)+150)
+                                        })
+                                    }
+                                }}
+                            />
+                        </View>
                         <LionsFooter isLoaded={true} />
                     </ScrollView>
                     < EYSFooter mySquadBtn={true} />
@@ -152,17 +100,11 @@ function bindAction(dispatch) {
     return {
         pushNewRoute:(route)=>dispatch(pushNewRoute(route)),
         replaceRoute:(route)=>dispatch(replaceRoute(route)),
-        setAccessGranted:(isAccessGranted)=>dispatch(setAccessGranted(isAccessGranted)),
-        setSquadToShow:(squad)=>dispatch(setSquadToShow(squad)),
-        setSquadData:(squad)=>dispatch(setSquadData(squad)),
     }
 }
 
 export default connect((state) => {
     return {
         detail: state.content.drillDownItem,
-        isAccessGranted: state.token.isAccessGranted,
-        squadToShow: state.squad.squadToShow,
-        squadData: state.squad.squadData,
     }
 }, bindAction)(MyLionsCoachProfile)
