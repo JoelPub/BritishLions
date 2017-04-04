f13f8fb65f9621f18725a486a3b592cffc0ca2c5
Joel <jwang2@apdgroup.com>
2017-04-04 15:01:22 +0800

    LIONS-498

--- js/components/utility/notification.js
+++ js/components/utility/notification.js
@@ -13,29 +13,17 @@ export const configure= new Promise((resolve,reject)=>{
 export function schedule() {
         let startDate=new Date()
         let offset=startDate.getTimezoneOffset()
-        // let targetDate1=new Date(Date.UTC(2017, 1, 7, 7, 0, 0))
-        // let targetDate2=new Date(Date.UTC(2017, 1, 14, 7, 0, 0))
         // console.log('!!!startDate',startDate)
         // console.log('!!!offset',offset)
-        // console.log('!!!targetDate1',targetDate1)
-        // console.log('!!!targetDate2',targetDate2)
-        // console.log('!!!',targetDate1-startDate)
-        // console.log('!!!',targetDate1-startDate+offset*60*1000)
-
-        // PushNotification.localNotificationSchedule({
-        //   message: "My Notification Message1", 
-        //   date: targetDate1
-        // })
-        // PushNotification.localNotificationSchedule({
-        //   message: "My Notification Message2", 
-        //   date: targetDate2
-        // })
         scheduleList.map((item,index)=>{
         	let targetDate=new Date(Date.UTC(item.year, item.month, item.day, item.hour, item.minute, item.second))
-        	if(targetDate-startDate+offset*60*1000>0) {
+        	// console.log('!!!targetDate',targetDate)
+         //    console.log('!!!',targetDate-startDate)
+         //    console.log('!!!',targetDate-startDate+offset*60*1000)
+            if(targetDate-startDate>0) {
 	        	PushNotification.localNotificationSchedule({
 		          message: item.message, 
-		          date: new Date(Date.UTC(item.year, item.month, item.day, item.hour, item.minute, item.second))
+		          date: targetDate
 		        })
         	}
         })
