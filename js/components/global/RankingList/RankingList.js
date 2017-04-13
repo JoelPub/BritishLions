b8130c43ce62e837c956f9c74cba7ceeaa19e034
Yilun Yang <yyang@apdgroup.com>
2017-04-13 14:05:25 +1000

    bug fixed for calendar

--- js/components/global/RankingList/RankingList.js
+++ js/components/global/RankingList/RankingList.js
@@ -220,7 +220,7 @@ const  Content = ({data,index,addStyle}) => {
 const NilGroup = (props) => (
   <View style={styles.nilView}>
     <Text style={styles.describeText} numberOfLines={3}>You have successfully created a private league.</Text>
-    <Text style={styles.describeText2} numberOfLines={3}>Your league table will be generated within the next 24 hours.</Text>
+    <Text style={styles.describeText2} numberOfLines={3}>Your league table will be generated within the next 4 hours.</Text>
   </View>
 )
 class RankingList extends Component {
