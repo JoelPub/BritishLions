eba1f1397eb8a3dcfe642c02cd85d0ccb8c64b23
roy anonuevo <ranonuevo@apdgroup.com>
2017-05-02 14:35:30 +0800

    LIONS-1045 Implement logic

--- js/components/landing/fixture.js
+++ js/components/landing/fixture.js
@@ -1,19 +1,20 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { Image, View, Text } from 'react-native'
+import { Image, View, Text, ActivityIndicator } from 'react-native'
 import { connect } from 'react-redux'
 import { drillDown } from '../../actions/content'
 import { Icon } from 'native-base'
 import { styleSheetCreate } from '../../themes/lions-stylesheet'
 import styleVar from '../../themes/variable'
 import styles from './styles'
-import { limitArrayList } from '../utility/helper'
+import { limitArrayList, strToLower } from '../utility/helper'
 import ButtonFeedback from '../utility/buttonFeedback'
 import ImagePlaceholder from '../utility/imagePlaceholder'
 import LinearGradient from 'react-native-linear-gradient'
 import LiveBox from '../global/liveBox'
 import shapes from '../../themes/shapes'
+import _fetch from '../utility/fetch'
 
 // For mapping a static image only, since require() is not working with concatenating a dynamic variable
 // should be delete this code once api is ready.
@@ -21,6 +22,9 @@ import fixturesList from '../../../contents/fixtures/data.json'
 import fixturesImages from '../../../contents/fixtures/images'
 
 const locStyle = styleSheetCreate({ 
+    activityIndicatorWrapper: {
+        padding: 20
+    },
     bannerDesc: {
         paddingTop: 7
     },
@@ -53,20 +57,96 @@ const locStyle = styleSheetCreate({
     },
 })
 
+const PageTitle = ({title}) => (
+    <View style={styles.pageTitle}>
+        <Text style={styles.pageTitleText}>
+            {title}
+        </Text>
+    </View>
+)
+
+const Banner = ({data, pressBanner}) => (
+    <ButtonFeedback
+        style={styles.banner}
+        onPress={pressBanner}>
+        <ImagePlaceholder height={200}>
+            <LinearGradient style={styles.fixtureImgContainer} colors={['#d9d7d8', '#FFF']}>
+                <Image
+                    resizeMode='contain'
+                    style={styles.bannerImg}
+                    source={data.image} />
+            </LinearGradient>
+        </ImagePlaceholder>
+        <View style={[shapes.triangle, {marginTop: -12}]} />
+        <View style={styles.bannerDetails}>
+            <Text style={styles.bannerTitle}>{ data.date }</Text>
+            <Text style={styles.bannerDesc}>{ data.title }</Text>
+        </View>
+    </ButtonFeedback>
+)
+
+const LiveGame = ({data, pressCoachBox, pressBanner}) => (
+    <View>
+        <PageTitle title='GAME NOW LIVE' />
+        
+        <LiveBox data={{}} inverse={true}/>
+        
+        <Banner data={data} pressBanner={pressBanner}/>
+        
+        <View style={[styles.bannerDetails, locStyle.bannerDetails]}>
+            <Text style={[styles.bannerDesc, locStyle.bannerDesc]}>Provincial Union vs British & Irish Lions</Text>
+        </View>
+        <View style={locStyle.infoBox}>
+            <Text style={locStyle.infoBoxText}>
+                Visit the British & Irish Lions Coach's Box for live match coverage and statistics.
+            </Text>
+            <ButtonFeedback 
+                rounded 
+                style={[styles.roundButton]}
+                onPress={pressCoachBox}
+            >
+                <Image resizeMode='contain' source={require('../../../contents/my-lions/squadLogo.png')}
+                    style={locStyle.logoIcon}/>
+                <Text ellipsizeMode='tail' numberOfLines={1} style={styles.roundButtonLabel} >
+                    COACH'S BOX
+                </Text>
+            </ButtonFeedback>
+        </View>
+    </View>
+)
+
+const PreGame = ({data, pressBanner}) => (
+    <View>
+        <PageTitle title='UPCOMING FIXTURE' />
+        <Banner data={data} pressBanner={pressBanner}/>
+    </View>
+)
+
+const PostGame = ({data, pressBanner}) => (
+    <View>
+        <PageTitle title='LAST FIXTURE' />
+        <Banner data={data} pressBanner={pressBanner}/>
+    </View>
+)
+
 class PlayerFigure extends Component {
     constructor(props){
         super(props)
 
         this.state = {
+            fixture: {},
+            isLoaded: false,
+
             fixturesList: [],
             fixturesLeft: [],
-            isGameLive: true
+            isGameLive: false
         }
     }
 
     componentDidMount() {
         setTimeout(() => {
-            this._fetchFixture()
+            //this._fetchFixture()
+            this._getFixturesInfo()
         }, 600)
     }
 
@@ -95,97 +175,53 @@ class PlayerFigure extends Component {
         })
     }
 
+    _getFixturesInfo() {
+         _fetch({url: 'http://bilprod-r4dummyapi.azurewebsites.net/GetFixturesInfo'}).then((res)=>{
+            //if (__DEV__) console.log('res', res)
+
+            // TEMPORARY: merge image FROM LOCAL UNTIL API IS NOT DONE
+            res.image = fixturesImages[res.id]
+            //res.game_status = 'live' // intercept
+
+            this.setState({
+                fixture: res,
+                isLoaded: true
+            })
+        }).catch((error)=>{
+            // if (__DEV__)console.log(error)
+        })
+    }
+
     _goToCoachBox = () => {
         this._drillDown('', 'coachBox')
     }
 
+    _gameMode(data) {
+        switch (strToLower(data.game_status)) {
+            case 'live':
+                return <LiveGame data={data} pressBanner={()=> this._drillDown(data, 'fixtureDetails')} pressCoachBox={this._goToCoachBox}/>
+                break;
+            case 'pre':
+                return <PreGame data={data} pressBanner={()=> this._drillDown(data, 'fixtureDetails')}/>
+                break;
+            case 'post':
+                return <PostGame data={data} pressBanner={()=> this._drillDown(data, 'fixtureDetails')}/>
+                break;
+            default:
+                return <View></View>
+        }
+    }
+
     render() {
+
         return (
             <View>
                 {
-                    this.state.isGameLive?
-                        <View>
-                            <View style={styles.pageTitle}>
-                                <Text style={styles.pageTitleText}>
-                                    GAME NOW LIVE
-                                </Text>
-                            </View>
-                            
-                            <LiveBox data={{}} inverse={true}/>
-                            
-                            <ButtonFeedback 
-                                style={styles.banner}
-                                onPress={() => this._drillDown(fixturesList[0], 'fixtureDetails')}>
-                                <ImagePlaceholder height={200}>
-                                    <LinearGradient style={styles.fixtureImgContainer} colors={['#d9d7d8', '#FFF']}>
-                                        <Image
-                                            resizeMode='contain'
-                                            style={styles.bannerImg}
-                                            source={fixturesImages[1]} />
-                                    </LinearGradient>
-                                </ImagePlaceholder>
-                            </ButtonFeedback>
-                            <View style={[styles.bannerDetails, locStyle.bannerDetails]}>
-                                <Text style={[styles.bannerDesc, locStyle.bannerDesc]}>Provincial Union vs British & Irish Lions</Text>
-                            </View>
-                            <View style={locStyle.infoBox}>
-                                <Text style={locStyle.infoBoxText}>
-                                    Visit the British & Irish Lions Coach's Box for live match coverage and statistics.
-                                </Text>
-                                <ButtonFeedback 
-                                    rounded 
-                                    style={[styles.roundButton]}
-                                    onPress={this._goToCoachBox}
-                                >
-                                    <Image resizeMode='contain' source={require('../../../contents/my-lions/squadLogo.png')}
-                                        style={locStyle.logoIcon}/>
-                                    <Text ellipsizeMode='tail' numberOfLines={1} style={styles.roundButtonLabel} >
-                                        COACH'S BOX
-                                    </Text>
-                                </ButtonFeedback>
-                            </View>
-                        </View>
+                    this.state.isLoaded?
+                        this._gameMode(this.state.fixture)
                     :
-                        <View>
-                            {
-                                this.state.fixturesList.length !== 0 && 
-                                <View>
-                                    <View style={styles.pageTitle}>
-                                        <Text style={styles.pageTitleText}>
-                                            {
-                                                this.state.fixturesList.length === 1? 'LAST FIXTURE' : 'UPCOMING FIXTURE'
-                                            }
-                                        </Text>
-                                    </View>
-                                    {
-                                        this.state.fixturesLeft.map(function(item, index) {
-                                            let date = item.date.toUpperCase() || ''
-                                            let title = item.title || ''
-                                            let image = fixturesImages[item.id]
-
-                                            return (
-                                                <ButtonFeedback key={index}
-                                                    style={styles.banner}
-                                                    onPress={() => this._drillDown(item, 'fixtureDetails')}>
-                                                    <ImagePlaceholder height={200}>
-                                                        <LinearGradient style={styles.fixtureImgContainer} colors={['#d9d7d8', '#FFF']}>
-                                                            <Image
-                                                                resizeMode='contain'
-                                                                style={styles.bannerImg}
-                                                                source={image} />
-                                                        </LinearGradient>
-                                                    </ImagePlaceholder>
-                                                    <View style={[shapes.triangle, {marginTop: -12}]} />
-                                                    <View style={styles.bannerDetails}>
-                                                        <Text style={styles.bannerTitle}>{ date }</Text>
-                                                        <Text style={styles.bannerDesc}>{ title }</Text>
-                                                    </View>
-                                                </ButtonFeedback>
-                                            )
-                                        }, this)
-                                    }
-                                </View>
-                            }
+                        <View style={locStyle.activityIndicatorWrapper}>
+                            <ActivityIndicator size='small' /> 
                         </View>
                 }
             </View>           
