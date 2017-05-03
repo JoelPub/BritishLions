dd14672705e0414d97c7a869681ae36b56023745
Joel <jwang2@apdgroup.com>
2017-05-03 11:15:21 +0800

    LIONS-1066

--- js/components/matchCenter/Component/momentum/index.js
+++ js/components/matchCenter/Component/momentum/index.js
@@ -13,7 +13,6 @@ import Fixture from '../../../utility/fixture'
 import LiveBox from '../../../global/liveBox'
 import loader from '../../../../themes/loader-position'
 import _fetch from '../../../utility/fetch'
-import { service } from '../../../utility/services'
 
 class Momentum extends Component {
 
@@ -21,39 +20,16 @@ class Momentum extends Component {
          super(props)
          this.state = {
               h:styleVar.deviceHeight-270,
-              data:[],
-              isLoaded:false,
-              isChanged:false
+              data:this.props.momentumData,
+              // isLoaded:false,
+              // isChanged:false
          }
-         this.fullTime=80
     }
-    componentWillReceiveProps(nextProps) {
-        // if (__DEV__)console.log('momentum componentWillReceiveProps nextProps.isActive',nextProps.isActive)
-        // if (__DEV__)console.log('momentum componentWillReceiveProps this.props.isActive',this.props.isActive)
-        if(nextProps.isActive&&!this.props.isActive) {
-        let optionsInfo = {
-            url: 'http://bilprod-r4dummyapi.azurewebsites.net/getGameMomentum',
-            data: {id:1},
-            onAxiosStart: null,
-            onAxiosEnd: null,
-            method: 'post',
-            onSuccess: (res) => {
-                // if (__DEV__)console.log('res',res)
-                if(res.data) {
-                    if (__DEV__)console.log('res.data',res.data)
-                        this.setState({isChanged:true},()=>{
-                            this.setState({data:this.processMomentumData(res.data.momentum),isLoaded:true})
-                        })
-                }
-            },
-            onError: ()=>{
-                this.setState({isLoaded:true})
-            },
-            isRequiredToken: false,
-            channel: 'EYC3',
-            isQsStringify:false
-        }
-        service(optionsInfo) 
+    // componentWillReceiveProps(nextProps) {
+    //     if (__DEV__)console.log('momentum componentWillReceiveProps nextProps.momentumData',nextProps.momentumData)
+    //     if (__DEV__)console.log('momentum componentWillReceiveProps this.props.momentumData',this.props.momentumData)
+    //     if(nextProps.momentumData.team_momentum) {
+        
         //     this.props.setHeight(this.state.h,'momentum')
             
         //     this.setState({isLoaded:false,isChanged:true},()=>{
@@ -75,38 +51,38 @@ class Momentum extends Component {
         //         }).catch((error)=>{
         //         })
         //     },5000) 
-        }
-    }
-    processMomentumData(data){
-        // if(__DEV__)console.log('processMomentumData')
-        let result=[]
-        for(let i=0;i<this.fullTime;i=i+10){
-            let momentum={score_advantage:[],team_momentum:[],isFirst:false,isLast:false,timeMark:0}
-            // if(__DEV__)console.log('momentum',momentum)
-            if(data.team_momentum.findIndex(x=>{
-                return parseInt(x.time)>i&&parseInt(x.time)<=i+10
-            })>-1) {
-                momentum.team_momentum=data.team_momentum.filter(x=>{
-                    return parseInt(x.time)>i&&parseInt(x.time)<=i+10
-                })
-                momentum.score_advantage=data.score_advantage.filter(x=>{
-                    return parseInt(x.time)===i||parseInt(x.time)===i+10
-                })
-                if (i===0) momentum.isFirst=true
-                if(data.team_momentum.findIndex(x=>{return parseInt(x.time)>i+10&&parseInt(x.time)<=i+20})===-1) momentum.isLast=true
-                momentum.timeMark=i+10
-                result.push(momentum)
+    //     }
+    // }
+    // processMomentumData(data){
+    //     // if(__DEV__)console.log('processMomentumData')
+    //     let result=[]
+    //     for(let i=0;i<this.fullTime;i=i+10){
+    //         let momentum={score_advantage:[],team_momentum:[],isFirst:false,isLast:false,timeMark:0}
+    //         // if(__DEV__)console.log('momentum',momentum)
+    //         if(data.team_momentum.findIndex(x=>{
+    //             return parseInt(x.time)>i&&parseInt(x.time)<=i+10
+    //         })>-1) {
+    //             momentum.team_momentum=data.team_momentum.filter(x=>{
+    //                 return parseInt(x.time)>i&&parseInt(x.time)<=i+10
+    //             })
+    //             momentum.score_advantage=data.score_advantage.filter(x=>{
+    //                 return parseInt(x.time)===i||parseInt(x.time)===i+10
+    //             })
+    //             if (i===0) momentum.isFirst=true
+    //             if(data.team_momentum.findIndex(x=>{return parseInt(x.time)>i+10&&parseInt(x.time)<=i+20})===-1) momentum.isLast=true
+    //             momentum.timeMark=i+10
+    //             result.push(momentum)
 
-            }
-            else {
-                result.push(null)
-            }
-        }
+    //         }
+    //         else {
+    //             result.push(null)
+    //         }
+    //     }
 
-        // if(__DEV__)console.log('result',result)
-        return result.reverse()
+    //     // if(__DEV__)console.log('result',result)
+    //     return result.reverse()
 
-    }
+    // }
     measurePage(page,event) {
         // if (__DEV__)console.log('momentum')
         const { x, y, width, height, } = event.nativeEvent.layout
@@ -117,26 +93,22 @@ class Momentum extends Component {
         // if (__DEV__)console.log('height',height)
         let h=y+180>styleVar.deviceHeight-360?y+180:styleVar.deviceHeight-360
         this.setState({h},()=>{
-            if(this.state.isChanged&&this.props.isActive) {
+            // if(this.state.isChanged&&this.props.isActive) {
                 this.props.setHeight(this.state.h,'momentum')
-                this.setState({isChanged:false})
-            }
+                // this.setState({isChanged:false})
+            // }
         })
         
     }
-    componentDidMount(){
+    // componentDidMount(){
 
-        this.props.setHeight(this.state.h,'momentum')
+    //     this.props.setHeight(this.state.h,'momentum')
 
-    }
+    // }
     
     render() {
         return (
-            <View>
-            {
-                this.props.isActive?
                 <View style={{marginTop:50,backgroundColor:'rgb(255,255,255)'}}>
-
                     <LiveBox data={{}} />
                     <View style={{paddingVertical:10,borderColor:'rgb(216, 217, 218)'}}>
                         <View style={{flexDirection:'row',paddingLeft:20,alignItems:'center',marginBottom:20}}>
@@ -148,38 +120,25 @@ class Momentum extends Component {
                                 <Icon name='ios-information-circle-outline' style={{color: styleVar.colorScarlet,fontSize: 22,lineHeight: 22}} />
                             </ButtonFeedback>
                         </View>
-                        {
-                            this.state.isLoaded?
-                                <View>
-                                    {
-                                        this.state.data.map((value,index)=>{
-                                            return (
-                                                <View key={index} >
-                                                    {value!==null?
-                                                        <MomentumTracker data={value} isHost={false}/>
-                                                        :
-                                                        null
-                                                    }
-                                                </View>
-                                                )
-                                            })
-                                    }
-                                    
-                                    <View onLayout={this.measurePage.bind(this,'momentum')} />
-                                </View>
-                            :
-                                <View>
-                                    <ActivityIndicator style={[loader.centered,{height:100}]} size='small' />
-
-                                    <View onLayout={this.measurePage.bind(this,'momentum')} />
-                                </View>
-                        }
+                        <View >
+                            {
+                                this.props.momentumData.map((value,index)=>{
+                                    return (
+                                        <View key={index} >
+                                            {value!==null?
+                                                <MomentumTracker data={value} isHost={false}/>
+                                                :
+                                                null
+                                            }
+                                        </View>
+                                        )
+                                    })
+                            }
+                            
+                            <View onLayout={this.measurePage.bind(this,'momentum')} />
+                        </View>
                     </View>
                 </View>
-                :
-                null
-            }
-            </View>
         )
     }
 }
