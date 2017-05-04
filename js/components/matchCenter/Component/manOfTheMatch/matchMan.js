39edd9d1ba46b1cbd5b57a34c76f81f16dbf9d85
Joel <jwang2@apdgroup.com>
2017-05-04 16:45:05 +0800

    LIONS-1066

--- js/components/matchCenter/Component/manOfTheMatch/matchMan.js
+++ js/components/matchCenter/Component/manOfTheMatch/matchMan.js
@@ -20,22 +20,10 @@ import Immutable, { Map, List,Iterable } from 'immutable'
 import { strToUpper,splitName,mapJSON } from '../../../utility/helper'
 
 
-const styles = styleSheetCreate({    
-    individaulPositionRow:{
-        flexDirection:'row',
-    },
+const styles = styleSheetCreate({
     posBtn:{
         borderLeftWidth:1,
         borderColor:'rgb(239, 239, 240)',
-    },
-    addPlayerIcon:{
-        fontSize:60,
-        color:'rgb(255,255,255)'
-    },
-    playerPositionTextWrapper:{
-        height:styleVar.deviceWidth*0.16,
-        justifyContent: 'center',
-        paddingTop:5
     },
     playerNameTextWrapper:{
         marginTop:-12,
@@ -53,15 +41,6 @@ const styles = styleSheetCreate({
             paddingTop: 12,
             paddingBottom: 6
         }
-    },
-    playerPositionText: {
-        textAlign: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontFamily: styleVar.fontCondensed,
-        fontSize: 18,
-        lineHeight: 18,
-        backgroundColor: 'transparent',
     },
     playerNameText: {
         color: '#FFF',
@@ -84,11 +63,6 @@ const styles = styleSheetCreate({
         width: styleVar.deviceWidth / 3,
         height: styleVar.deviceWidth / 3
     },
-    indivPlayerImage:{
-        backgroundColor: '#FFF',
-        width: styleVar.deviceWidth / 2,
-        height: styleVar.deviceWidth / 2
-    },
     posTitle:{
         flexDirection:'row',
         justifyContent:'space-between',
@@ -109,42 +83,6 @@ const styles = styleSheetCreate({
         fontSize:24,
         lineHeight:24
     },
-    posTitleLeft:{
-        color:'rgb(175,0,30)',
-        textAlign:'left',
-        fontFamily: styleVar.fontCondensed,
-        fontSize:24,
-        lineHeight:24,
-    },
-    posTitleRight:{
-        color:'rgb(175,0,30)',
-        textAlign:'right',
-        fontFamily: styleVar.fontCondensed,
-        fontSize:24,
-        lineHeight:24,
-    },
-    indivPosition:{
-        width:styleVar.deviceWidth/2+1,
-        backgroundColor:'rgb(255,255,255)',
-        marginLeft:-1
-    },
-    indivPosTitle:{
-        borderTopWidth:1,
-        borderLeftWidth:1,
-        borderBottomWidth:1,
-        borderColor:'rgb(216,217,218)',
-        height:50,
-        paddingTop:17,
-        backgroundColor:'rgb(239,239,240)',
-        borderRightWidth:1
-    },
-    indivPosTitleText:{
-        color:'rgb(175,0,30)',
-        textAlign:'center',
-        fontFamily: styleVar.fontCondensed,
-        fontSize:24,
-        lineHeight:24,
-    },
     posSwiperRow:{
         flexDirection:'row',
         backgroundColor:'black',
@@ -153,30 +91,6 @@ const styles = styleSheetCreate({
     posWrapper:{
         width:styleVar.deviceWidth/3+1,
         marginLeft:-1
-    },
-    posAddWrapper:{
-        width:styleVar.deviceWidth / 3,
-        height:styleVar.deviceWidth / 3,
-        backgroundColor:'rgb(175,0,30)',
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    subject:{
-    	backgroundColor: styleVar.colorText,
-        paddingTop: 20,
-        paddingBottom: 6,
-    },
-    subjectText: {
-    	alignSelf: 'center',
-        fontFamily: styleVar.fontCondensed,
-        fontSize: 28,
-        lineHeight: 28,
-        color: '#FFF',
-        backgroundColor: 'transparent',
-        textAlign:'center',
-        android: {
-            paddingBottom: 5
-        }
     },
     paginationDot: {
         backgroundColor: 'rgba(255, 255, 255, 0.3)', 
@@ -196,7 +110,7 @@ const styles = styleSheetCreate({
     }
 })
 
-const PlayerImgCell =({data,onPress}) =>(
+const PlayerImgCell =({data,selected,onPress}) =>(
     <ButtonFeedback onPress={onPress} style={styles.posBtn}>
         <ImagePlaceholder 
             width = {styleVar.deviceWidth / 3}
@@ -207,8 +121,8 @@ const PlayerImgCell =({data,onPress}) =>(
                 style={styles.playerImage} />
         </ImagePlaceholder>
         <View style={styles.playerNameTextWrapper}>
-            <View style={[shapes.triangle]} />
-            <View style={styles.titleBox}>
+            <View style={[shapes.triangle,selected&&{borderBottomColor:'rgb(9,127,64)'}]} />
+            <View style={[styles.titleBox,selected&&{backgroundColor:'rgb(9,127,64)'}]}>
                 <Text style={styles.playerNameText} numberOfLines={1}>{data.info.name&&data.info.name.toUpperCase().substring(0, data.info.name.lastIndexOf(" "))}</Text>
                 <Text style={styles.playerNameText} numberOfLines={1}>{data.info.name&&data.info.name.toUpperCase().substring(data.info.name.lastIndexOf(" ")+1, data.info.name.length)}</Text>
             </View>
@@ -228,8 +142,11 @@ class MatchMan extends Component {
 			this.uniondata = Data
 			this.state = {
 			    isLoaded: false,
-			    gameDayTeam: {},
+			    matchMan: {},
 			    isNetwork: true,
+                selectedPosition:null,
+                selectedSequence:null
+
 			}
     }
 
@@ -250,13 +167,6 @@ class MatchMan extends Component {
         }
     }
 
-    _showDetail(item, route,playerPos,max,seq) {
-			if (__DEV__)console.log('item',item)
-			if (__DEV__)console.log('route',route)
-			if (__DEV__)console.log('this.state.details',this.state.details)
-			this.props.drillDown(item, route)
-    }
-
     componentDidMount() {
         setTimeout(() => this._getSquad(), 600)
     }
@@ -272,7 +182,7 @@ class MatchMan extends Component {
                             let showSquadFeed=convertSquadToShow(MatchManModel(catchedOfficialSquad),catchedFullPlayerList,this.uniondata)
                             if (__DEV__)console.log('showSquadFeed',showSquadFeed.toJS())
                             // this.props.setOfficialSquadToShow(showSquadFeed.toJS())
-                            this.setState({gameDayTeam:showSquadFeed.toJS()},()=>{
+                            this.setState({matchMan:showSquadFeed.toJS()},()=>{
                                 this.setState({isLoaded:true})
                             })
                             
@@ -295,13 +205,29 @@ class MatchMan extends Component {
       })
     }
 
+    _selectMan(position,seq,info) {
+            if (__DEV__)console.log('position',position)
+            if (__DEV__)console.log('seq',seq)
+            let p=null
+            let s=null
+            let i=null
+            if(position!==this.state.selectedPosition||seq!==this.state.selectedSequence) {
+                p=position
+                s=seq
+                i=info
+            }
+            this.setState({selectedPosition:p,selectedSequence:s},()=>{
+                this.props.selectMan(info)
+            })
+    }
+
 	render() {
 		return (
 			<View>
 				{
 	                this.state.isLoaded?
 	                <View>
-		                <PositionTitle pos='FORWARDS' data={this.state.gameDayTeam.forwards}/>
+		                <PositionTitle pos='FORWARDS' data={this.state.matchMan.forwards}/>
 		                <Swiper
 		                height={styleVar.deviceWidth*0.63}
                         loop={true}
@@ -309,14 +235,14 @@ class MatchMan extends Component {
                         activeDot={<View style={[styles.paginationDot, styles.paginationDotActive]} />}
                         paginationStyle={styles.paginationBottom}>
 		                    {
-		                        mapJSON(this.state.gameDayTeam.forwards,3).map((rowData,i)=>{
+		                        mapJSON(this.state.matchMan.forwards,3).map((rowData,i)=>{
 		                            return(
 		                                <View style={styles.posSwiperRow} key={i}>
 		                                    {
 		                                        rowData.map((item,index)=>{
 		                                            return(
 		                                                    <View style={styles.posWrapper} key={index}>
-		                                                            <PlayerImgCell data={item} onPress = {() => this._showDetail(item.info,'myLionsPlayerProfile')}/>
+		                                                            <PlayerImgCell data={item} selected={this.state.selectedPosition==='forwards'&&this.state.selectedSequence===index} onPress = {() => this._selectMan('forwards',index,item.info)}/>
 		                                                    </View>
 		                                                )
 		                                        }, this)
@@ -328,7 +254,7 @@ class MatchMan extends Component {
 		                    }
 		                </Swiper>
 		                
-		                <PositionTitle pos='BACKS' data={this.state.gameDayTeam.backs}/>
+		                <PositionTitle pos='BACKS' data={this.state.matchMan.backs}/>
 		                <Swiper
 		                height={styleVar.deviceWidth*0.63}
                         loop={true}
@@ -336,14 +262,14 @@ class MatchMan extends Component {
                         activeDot={<View style={[styles.paginationDot, styles.paginationDotActive]} />}
                         paginationStyle={styles.paginationBottom}>
 		                    {
-		                        mapJSON(this.state.gameDayTeam.backs,3).map((rowData,i)=>{
+		                        mapJSON(this.state.matchMan.backs,3).map((rowData,i)=>{
 		                            return(
 		                                <View style={styles.posSwiperRow} key={i}>
 		                                    {
 		                                        rowData.map((item,index)=>{
 		                                            return(
 		                                                <View style={styles.posWrapper} key={index}>
-		                                                        <PlayerImgCell data={item} onPress = {() => this._showDetail(item.info,'myLionsPlayerProfile')}/>
+		                                                        <PlayerImgCell data={item} selected={this.state.selectedPosition==='backs'&&this.state.selectedSequence===index} onPress = {() => this._selectMan('backs',index,item.info)}/>
 		                                                </View>
 		                                                )
 		                                        }, this)
