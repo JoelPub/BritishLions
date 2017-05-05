895bf1a478312ccabea785523cd2b703cd386108
周贤 <zhouxian@xiandeMacBook-Pro.local>
2017-05-05 13:26:21 +0800

    LIONS-1097

--- js/components/global/Carousel/static/entries.js
+++ js/components/global/Carousel/static/entries.js
@@ -5,28 +5,23 @@ export const ENTRIES1 = [
     illustration: 'http://i.imgur.com/UYiroysl.jpg'
   },
   {
-    title: 'MATCH SUMMARY',
+    title: 'MOMENTUM',
     subtitle: 'Lorem ipsum dolor sit amet, consectet adipiscing elit. Pellentesque gravida.',
     illustration: 'http://i.imgur.com/UPrs1EWl.jpg'
   },
   {
-    title: 'MATCH SUMMARY',
+    title: 'SET PLAYS',
     subtitle: 'Lorem ipsum dolor sit amet, consectet adipiscing elit. Pellentesque gravida. ',
     illustration: 'http://i.imgur.com/MABUbpDl.jpg'
   },
   {
-    title: 'MATCH SUMMARY',
+    title: 'ON FIRE',
     subtitle: 'Lorem ipsum dolor sit amet, consectet adipiscing elit. Pellentesque gravida.',
     illustration: 'http://i.imgur.com/KZsmUi2l.jpg'
   },
   {
-    title: 'MATCH SUMMARY',
+    title: 'MAN OF THE MATCH',
     subtitle: 'Lorem ipsum dolor sit amet, consectet adipiscing elit. Pellentesque gravida.',
     illustration: 'http://i.imgur.com/2nCt3Sbl.jpg'
   },
-  {
-    title: 'MATCH SUMMARY',
-    subtitle: 'Lorem ipsum dolor sit amet, consectet adipiscing elit. Pellentesque gravida.',
-    illustration: 'http://i.imgur.com/lceHsT6l.jpg'
-  }
 ];
