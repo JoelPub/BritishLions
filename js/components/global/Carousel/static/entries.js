b4e4c2a80a77ec5d426372932ecb99e84def370f
周贤 <zhouxian@xiandeMacBook-Pro.local>
2017-05-24 16:57:09 +0800

    LIONS-1248  update Image

--- js/components/global/Carousel/static/entries.js
+++ js/components/global/Carousel/static/entries.js
@@ -1,33 +1,29 @@
 
-import image1 from './images/matchSummary.jpg'
-import image2 from './images/ setPlays.jpg'
-import image3 from './images/topLions.jpg'
-import image4 from './images/ manOfTheMatch.jpg'
-import image5 from './images/gameDayTeam.jpg'
+
 export const ENTRIES1 = [
   {
     title: 'MATCH SUMMARY',
     subtitle: 'Live commentary for the current match.',
-    illustration: image1
+    illustration: '../static/images/matchSummary.jpg'
   },
   {
     title: 'RUN OF PLAY',
     subtitle: 'See which team is dominating play based on match statistics compared to the score difference.',
-    illustration: image2
+    illustration: '../static/images/ setPlays.jpg'
   },
   {
     title: 'SET PLAYS',
     subtitle: 'See where on the pitch Set Plays are taking place.',
-    illustration: image3
+    illustration: '../static/images/topLions.jpg'
   },
   {
     title: 'TOP LIONS',
     subtitle: 'See what Players are performing above their career averages.',
-    illustration: image4
+    illustration: '../static/images/ manOfTheMatch.jpg'
   },
   {
     title: 'MAN OF THE MATCH',
     subtitle: 'Vote for your Man of the Match.',
-    illustration: image5
+    illustration: '../static/images/gameDayTeam.jpg'
   },
 ];
